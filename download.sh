c6d488ab0a7af52e24417bc29881cdb519b36a5a
axcellsz <122135545+axcellsz@users.noreply.github.com>
2023-01-11 13:11:02 +0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-wget -O /usr/bin/ad-cert https://github.com/axcellsz/sec/raw/main/file/ad-cert && chmod +x /usr/bin/ad-cert
-wget -O /usr/bin/ad-dom https://github.com/axcellsz/sec/raw/main/file/ad-dom && chmod +x /usr/bin/ad-dom
-wget -O /usr/bin/cmenu https://github.com/axcellsz/sec/raw/main/file/cmenu && chmod +x /usr/bin/cmenu
+#wget -O /usr/bin/ad-cert https://github.com/axcellsz/sec/raw/main/file/ad-cert && chmod +x /usr/bin/ad-cert
+#wget -O /usr/bin/ad-dom https://github.com/axcellsz/sec/raw/main/file/ad-dom && chmod +x /usr/bin/ad-dom
+#wget -O /usr/bin/cmenu https://github.com/axcellsz/sec/raw/main/file/cmenu && chmod +x /usr/bin/cmenu
 wget -O /usr/bin/set-cmenu https://github.com/axcellsz/sec/raw/main/file/set-cmenu && chmod +x /usr/bin/set-cmenu
-wget -O /usr/bin/vms-akb https://github.com/axcellsz/sec/raw/main/file/vms-akb && chmod +x /usr/bin/vms-akb
-wget -O /usr/bin/vms-def https://github.com/axcellsz/sec/raw/main/file/vms-def && chmod +x /usr/bin/vms-def
-wget -O /usr/bin/vms-flx https://github.com/axcellsz/sec/raw/main/file/vms-flx && chmod +x /usr/bin/vms-flx
-wget -O /usr/bin/vms-man https://github.com/axcellsz/sec/raw/main/file/vms-man && chmod +x /usr/bin/vms-man
+#wget -O /usr/bin/vms-akb https://github.com/axcellsz/sec/raw/main/file/vms-akb && chmod +x /usr/bin/vms-akb
+#wget -O /usr/bin/vms-def https://github.com/axcellsz/sec/raw/main/file/vms-def && chmod +x /usr/bin/vms-def
+#wget -O /usr/bin/vms-flx https://github.com/axcellsz/sec/raw/main/file/vms-flx && chmod +x /usr/bin/vms-flx
+#wget -O /usr/bin/vms-man https://github.com/axcellsz/sec/raw/main/file/vms-man && chmod +x /usr/bin/vms-man
