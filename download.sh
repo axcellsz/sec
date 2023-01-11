f1f72bbe4a760cfb1572985494b5f30d0087717d
axcellsz <122135545+axcellsz@users.noreply.github.com>
2023-01-11 16:35:56 +0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm -rf /usr/bin/menu
 rm -rf /usr/bin/set-cmenu
+rm -rf /usr/bin/vms-akb
 #wget -O /usr/bin/ad-cert https://github.com/axcellsz/sec/raw/main/file/ad-cert && chmod +x /usr/bin/ad-cert
 #wget -O /usr/bin/ad-dom https://github.com/axcellsz/sec/raw/main/file/ad-dom && chmod +x /usr/bin/ad-dom
 wget -O /usr/bin/menu https://github.com/axcellsz/sec/raw/main/file/menu && chmod +x /usr/bin/menu
