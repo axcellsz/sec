650a258ae7250fe0816ea628a864a2c2e029f2d6
axcellsz <122135545+axcellsz@users.noreply.github.com>
2023-01-13 09:47:09 +0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-rm -rf /usr/bin/menu
-rm -rf /usr/bin/set-cmenu
-rm -rf /usr/bin/vms-akb
+#rm -rf /usr/bin/menu
+#rm -rf /usr/bin/set-cmenu
+#rm -rf /usr/bin/vms-akb
 #wget -O /usr/bin/ad-cert https://github.com/axcellsz/sec/raw/main/file/ad-cert && chmod +x /usr/bin/ad-cert
 #wget -O /usr/bin/ad-dom https://github.com/axcellsz/sec/raw/main/file/ad-dom && chmod +x /usr/bin/ad-dom
-wget -O /usr/bin/menu https://github.com/axcellsz/sec/raw/main/file/menu && chmod +x /usr/bin/menu
-wget -O /usr/bin/set-cmenu https://github.com/axcellsz/sec/raw/main/file/set-cmenu && chmod +x /usr/bin/set-cmenu
-wget -O /usr/bin/vms-akb https://github.com/axcellsz/sec/raw/main/file/vms-akb && chmod +x /usr/bin/vms-akb
+#wget -O /usr/bin/menu https://github.com/axcellsz/sec/raw/main/file/menu && chmod +x /usr/bin/menu
+#wget -O /usr/bin/set-cmenu https://github.com/axcellsz/sec/raw/main/file/set-cmenu && chmod +x /usr/bin/set-cmenu
+#wget -O /usr/bin/vms-akb https://github.com/axcellsz/sec/raw/main/file/vms-akb && chmod +x /usr/bin/vms-akb
 #wget -O /usr/bin/vms-def https://github.com/axcellsz/sec/raw/main/file/vms-def && chmod +x /usr/bin/vms-def
-#wget -O /usr/bin/vms-flx https://github.com/axcellsz/sec/raw/main/file/vms-flx && chmod +x /usr/bin/vms-flx
+wget -O /usr/bin/vms-flx https://github.com/axcellsz/sec/raw/main/file/vms-flx && chmod +x /usr/bin/vms-flx
 #wget -O /usr/bin/vms-man https://github.com/axcellsz/sec/raw/main/file/vms-man && chmod +x /usr/bin/vms-man
 wget -O /usr/bin/set-akun https://github.com/axcellsz/sec/raw/main/file/set-akun && chmod +x /usr/bin/set-akun
